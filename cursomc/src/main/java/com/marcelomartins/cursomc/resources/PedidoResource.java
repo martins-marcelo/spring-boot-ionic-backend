e5165cb8d3765722f46ccf40e58a3498f6a7b97d
martins-marcelo <bfmarcelo1@gmail.com>
2020-05-13 16:25:18 -0500

    Endpoint /pedidos/{id} disponibilizado

---
+++ cursomc/src/main/java/com/marcelomartins/cursomc/resources/PedidoResource.java
@@ -0,0 +1,29 @@
+package com.marcelomartins.cursomc.resources;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelomartins.cursomc.domain.Pedido;
+import com.marcelomartins.cursomc.services.PedidoService;
+
+@RestController //Definindo que este é um controlador rest
+@RequestMapping(value="/pedidos") //Adicionando endpoint ao controlador
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //Adicionando verbo http para dizer que este é um metodo rest
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	
+}
