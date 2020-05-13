f29ce5683ab84585f2a4ddbf2c77e94875465e93
martins-marcelo <bfmarcelo1@gmail.com>
2020-05-13 16:35:14 -0500

    Utilizando apenas JsonIgnore

--- cursomc/src/main/java/com/marcelomartins/cursomc/domain/Endereco.java
+++ cursomc/src/main/java/com/marcelomartins/cursomc/domain/Endereco.java
@@ -7,10 +7,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco implements Serializable{
@@ -25,7 +24,7 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
