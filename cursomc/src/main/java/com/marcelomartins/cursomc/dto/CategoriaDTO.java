bb09981fbcd4aae10add2939649218b2ec089324
martins-marcelo <bfmarcelo1@gmail.com>
2020-07-10 13:10:35 -0500

    Validacao com BeanValidation

--- cursomc/src/main/java/com/marcelomartins/cursomc/dto/CategoriaDTO.java
+++ cursomc/src/main/java/com/marcelomartins/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,20 @@ package com.marcelomartins.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.marcelomartins.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+
 	private Integer id;
+	
+	@NotEmpty(message="preenchimento obrigatório")
+	@Length(min=5, max=80, message="o tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- cursomc/src/main/java/com/marcelomartins/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/marcelomartins/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcelomartins.cursomc.domain.Categoria;
+import com.marcelomartins.cursomc.dto.CategoriaDTO;
 import com.marcelomartins.cursomc.repositories.CategoriaRepository;
 import com.marcelomartins.cursomc.services.exceptions.DataIntegrityException;
 import com.marcelomartins.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,5 +53,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
