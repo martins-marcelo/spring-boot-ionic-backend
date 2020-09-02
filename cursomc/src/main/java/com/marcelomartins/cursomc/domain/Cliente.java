b3775583621fd85e060615a6f1b5869c2bb609f2
martins-marcelo <bfmarcelo1@gmail.com>
2020-09-02 09:11:15 -0500

    PUT, DELETE e GET para Cliente

--- cursomc/src/main/java/com/marcelomartins/cursomc/domain/Cliente.java
+++ cursomc/src/main/java/com/marcelomartins/cursomc/domain/Cliente.java
@@ -52,7 +52,7 @@ public class Cliente implements Serializable{
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCodigo();
+		this.tipo = (tipo == null) ? null : tipo.getCodigo();
 	}
 
 
